7215c3f47421960f7e7d0a2a702864fa081e36c8
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-12 14:08:57 -0400

    add remove item from cart logic

--- pages/cart.js
+++ pages/cart.js
@@ -9,6 +9,10 @@ export default function CartScreen() {
     const { state, dispatch } = useContext(Store);
     const { cart: { cartItems } } = state;
 
+    const removeItemHandler = (item) => {
+        dispatch({ type: 'CART_REMOVE_ITEM', payload: item })
+    }
+
     return (
         <Layout title="Shopping Cart">
             <h1 className='mb-4 text-xl'>Shopping Cart</h1>
@@ -46,7 +50,7 @@ export default function CartScreen() {
                                             <td className='p-5 text-right'>{item.quantity}</td>
                                             <td className='p-5 text-right'>${item.price}</td>
                                             <td className='p-5 text-center'>
-                                                <button>
+                                                <button onClick={() => removeItemHandler(item)}>
                                                     <XCircleIcon className="h-5 w-5" />
                                                 </button>
                                             </td>
