e13fecd84e2d5be5b6b1a9058eb9dc556b23894e
Leo Cheung <leo.cheung@cbsinteractive.com>
2022-09-12 11:26:51 -0400

    populate product details

---
+++ pages/product/[slug].js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import data from "../../utils/data";
+
+export default function ProductScreen() {
+    const { query } = useRouter();
+    const { slug } = query;
+
+    const product = data.products.find(item => item.slug === slug)
+    if (!product) {
+        return <div>Product Not Found</div>
+    }
+
+    return (
+        <Layout title={product.name}>
+            <div className="py-2">
+                <Link href="/">back to products</Link>
+            </div>
+            <div className="grid md:grid-cols-4 md:gap-3">
+                <div className="md:col-span-2">
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        width={640}
+                        height={640}
+                        layout="responsive"
+                    />
+                </div>
+
+                <div>
+                    <ul>
+                        <li><h1 className="text-lg">{product.name}</h1></li>
+                    </ul>
+                </div>
+            </div>
+        </Layout>
+    )
+}
