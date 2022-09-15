89610578c54a63ed2d96df77b31bf8faab988907
Leo <leoc2628@gmail.com>
2022-09-15 11:20:26 -0400

    import session and router to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
