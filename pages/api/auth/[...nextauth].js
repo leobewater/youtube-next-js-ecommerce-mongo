f820ec18016eef83cc95cb6a8b0cd8847df0098f
Leo <leoc2628@gmail.com>
2022-09-15 11:20:51 -0400

    fix jwt callback with return

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,7 @@ export const authOptions = {
     async jwt({ token, user }) {
       if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      return token;
     },
     async session({ session, token }) {
       if (token?._id) session.user._id = token._id;
@@ -30,8 +31,9 @@ export const authOptions = {
         await db.disconnect();
 
         if (user && bcryptjs.compareSync(credentials.password, user.password)) {
+          // console.log(user);
           return {
-            _id: user.id,
+            _id: user._id,
             name: user.name,
             email: user.email,
             image: 'f',
