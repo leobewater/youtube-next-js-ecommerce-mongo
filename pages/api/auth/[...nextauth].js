420167bddfdd38e9e58d710cd0549e14e6bd8cd7
Leo <leoc2628@gmail.com>
2022-09-15 10:17:29 -0400

    set up nextauth for email and password authentication

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,48 @@
+import bcryptjs from 'bcryptjs';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+
+export const authOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      if (user?.isAdmin) token.isAdmin = user.isAdmin;
+    },
+    async session({ session, token }) {
+      if (token?._id) session.user._id = token._id;
+      if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
+  providers: [
+    // connect to mongodb for user authentication
+    CredentialsProvider({
+      async authorize(credentials) {
+        await db.connect();
+        const user = await User.findOne({
+          email: credentials.email,
+        });
+        await db.disconnect();
+
+        if (user && bcryptjs.compareSync(credentials.password, user.password)) {
+          return {
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            image: 'f',
+            isAdmin: user.isAdmin,
+          };
+        }
+
+        throw new Error('Invalid email or password');
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
