97cef78bb1848821c3b29223ada979492fb05916
Leo <leoc2628@gmail.com>
2022-09-15 16:51:51 -0400

    add products seeder

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,14 +1,21 @@
 import db from '../../utils/db';
 import User from '../../models/User';
+import Product from '../../models/Product';
 import data from '../../utils/data';
 
-// reset user collection
 const handler = async (req, res) => {
   await db.connect();
+
+  // reset user collection
   await User.deleteMany();
   await User.insertMany(data.users);
+
+  // reset product collection
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
+
   await db.disconnect();
-  res.send({ message: 'seeded successfully' });
+  res.send({ message: 'Users and Products data seeded successfully' });
 };
 
 export default handler;
