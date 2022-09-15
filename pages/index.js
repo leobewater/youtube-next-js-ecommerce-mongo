fb5613925bbf40e839dc43c50fe951c7b910cb8f
Leo <leoc2628@gmail.com>
2022-09-15 17:22:00 -0400

    enable homepage add to cart button

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,44 @@
+import axios from 'axios';
+import { useContext } from 'react';
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
 import Product from '../models/Product';
 import db from '../utils/db';
+import { Store } from '../utils/Store';
+import { toast } from 'react-toastify';
 
 export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    // fetch the product id to the database for live price and stock data
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    // check stock amount
+    if (data.countInStock < quantity) {
+      toast.error('Sorry. Product is out of stock!');
+      return;
+    }
+
+    // fire event when adding item to cart
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product added to the cart');
+  };
+
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
-          <ProductItem product={product} key={product.slug} />
+          <ProductItem
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          />
         ))}
       </div>
     </Layout>
@@ -23,7 +53,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      products: products.map(db.convertDocToObj)
+      products: products.map(db.convertDocToObj),
     },
   };
 }
