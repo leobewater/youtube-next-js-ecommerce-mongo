f4f6294972a8d280a8c402f212a0e21abb092fb1
Leo <leoc2628@gmail.com>
2022-09-15 10:03:28 -0400

    add mongoose user model

---
+++ models/User.js
@@ -0,0 +1,14 @@
+// mongo model
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: String, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
