e276452fe460152fb166087c0c0e8c2d5ee7b210
Leo <leoc2628@gmail.com>
2022-09-16 08:03:00 -0400

    set up place order screen

---
+++ pages/placeorder.js
@@ -0,0 +1,133 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useContext } from 'react';
+import CheckoutWizard from '../components/CheckoutWizard';
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+
+function PlaceOrderScreen() {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { cartItems, shippingAddress, paymentMethod } = cart;
+
+  return (
+    <Layout title="Place Order">
+      <CheckoutWizard activeStep={3} />
+      <h1 className="mb-4 text-xl">Place Order</h1>
+      {cartItems.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <div className="card p-5">
+              <h2 className="mb-2 text-xl">Shipping Address</h2>
+              <div>
+                {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                {shippingAddress.city}, {shippingAddress.postalCode},{' '}
+                {shippingAddress.country}
+              </div>
+              <div>
+                <Link href="/shipping">Edit</Link>
+              </div>
+            </div>
+
+            <div className="card p-5">
+              <h2 className="mb-2 text-xl">Payment Method</h2>
+              <div>{paymentMethod}</div>
+              <div>
+                <Link href="/payment">Edit</Link>
+              </div>
+            </div>
+
+            <div className="card p-5">
+              <h2 className="mb-2 text-xl">Order Items</h2>
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="p-5 text-left">Item</th>
+                    <th className="p-5 text-right">Quantity</th>
+                    <th className="p-5 text-right">Price</th>
+                    <th className="p-5 text-right">Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item) => (
+                    <tr key={item.slug} className="border-b">
+                      <td>
+                        <Link href={`/product/${item.slug}`}>
+                          <a className="flex items-center">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                            />
+                            &nbsp;
+                            {item.name}
+                          </a>
+                        </Link>
+                      </td>
+                      <td className="p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">
+                        ${item.quantity * item.price}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div>
+                <Link href="/cart">Edit</Link>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="card p-5">
+              <h2 className="mb-2 text-lg">Order Summary</h2>
+              <ul>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Item</div>
+                    <div>${itemsPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Tax</div>
+                    <div>${taxPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Shipping</div>
+                    <div>${shippingPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Total</div>
+                    <div>${totalPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <button
+                    disabled={loading}
+                    onClick={placeOrderHandler}
+                    className="primary-button w-full"
+                  >
+                    {loading ? 'Loading...' : 'Place Order'}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+}
+
+export default PlaceOrderScreen;
