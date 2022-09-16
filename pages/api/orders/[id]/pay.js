e5e852386bd563e7dfcb73ac4f6888d86ce3253d
Leo <leoc2628@gmail.com>
2022-09-16 10:00:22 -0400

    move order id api to own folder and add paypal pay api endpoint

---
+++ pages/api/orders/[id]/pay.js
@@ -0,0 +1,38 @@
+import { getSession } from 'next-auth/react';
+import Order from '../../../../models/Order';
+import db from '../../../../utils/db';
+
+const handler = async (req, res) => {
+  const session = await getSession({ req });
+  // throw 401 errors when request has no session
+  if (!session) {
+    return res.status(401).send('signin is required');
+  }
+
+  await db.connect();
+  const order = await Order.findById(req.query.id);
+  if (order) {
+    if (order.isPaid) {
+      return res.status(400).send({ message: 'Error: order is already paid' });
+    }
+
+    order.isPaid = true;
+    order.paidAt = Date.now();
+
+    // from paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      email_address: req.body.email_address,
+    };
+
+    const paidOrder = await order.save();
+    await db.disconnect();
+    res.send({ message: 'order paid successfully', order: paidOrder });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Error: order not found' });
+  }
+};
+
+export default handler;
